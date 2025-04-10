a48e6f256a033dbf408f6edbd2861f9625c2916f
geoffrey1330 <israelgeoffrey13@gmail.com>
2025-04-10 02:18:07 +0100

    added uuidgen step to ENTRYPOINT

--- deploy/image/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ ! -f /var/lib/nvme/hostid ]; then
-  uuidgen > /var/lib/nvme/hostid
-fi
-cp /var/lib/nvme/hostid /etc/nvme/hostid
-echo "nqn.2014-08.org.nvmexpress:uuid:$(cat /etc/nvme/hostid)" > /etc/nvme/hostnqn
-
-exec /usr/local/bin/spdkcsi
-
