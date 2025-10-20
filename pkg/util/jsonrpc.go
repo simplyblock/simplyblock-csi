a846c970e16c2110b9f9b48f381f59f40f9fcf5b
geoffrey1330 <israelgeoffrey13@gmail.com>
2025-10-20 17:02:16 +0100

    get the target type from sbcli controlplane

--- pkg/util/jsonrpc.go
+++ pkg/util/jsonrpc.go
@@ -112,7 +112,7 @@ type LvolConnectResp struct {
 	NrIoQueues     int    `json:"nr-io-queues"`
 	CtrlLossTmo    int    `json:"ctrl-loss-tmo"`
 	Port           int    `json:"port"`
-	TargetType     string `json:"target"`
+	TargetType     string `json:"transport"`
 	IP             string `json:"ip"`
 	Connect        string `json:"connect"`
 	NSID           int    `json:"ns_id"`
@@ -311,7 +311,7 @@ func (client *RPCClient) getVolumeInfo(lvolID string) (map[string]string, error)
 		"nrIoQueues":     strconv.Itoa(result[0].NrIoQueues),
 		"ctrlLossTmo":    strconv.Itoa(result[0].CtrlLossTmo),
 		"model":          model,
-		"targetType":     result[0].TargetType,
+		"TargetType":     result[0].TargetType,
 		"connections":    string(connectionsData),
 		"nsId":           strconv.Itoa(result[0].NSID),
 	}, nil
