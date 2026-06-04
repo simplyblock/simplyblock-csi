3b48a80377873c7cc849618c21ebbd371f3df6de
geoffrey1330 <israelgeoffrey13@gmail.com>
2026-06-04 14:05:28 +0100

    fix: detect volume-exists response by substring match to handle name-embedded error messages

--- pkg/util/jsonrpc.go
+++ pkg/util/jsonrpc.go
@@ -225,7 +225,7 @@ func (client APIClient) createVolume(ctx context.Context, poolID string, params 
 	if err != nil {
 		if errorMatches(err, ErrJSONNoSpaceLeft) {
 			err = ErrJSONNoSpaceLeft
-		} else if errorMatches(err, ErrJSONVolumeExists) || strings.Contains(strings.ToLower(err.Error()), "conflict") {
+		} else if strings.Contains(strings.ToLower(err.Error()), "exists") || strings.Contains(strings.ToLower(err.Error()), "conflict") {
 			err = ErrJSONVolumeExists
 		}
 		return "", err
